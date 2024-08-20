f87db9a7baabff705dc5653db51233ce2f586290
Taufan Prasetyo <taufanremote@gmail.com>
2024-08-20 12:56:26 +0700

    Data Type: string

---
+++ bin/string.dart
@@ -0,0 +1,21 @@
+void main() {
+  String firstName = 'Taufan';
+  String lastName = 'Prasetyo';
+
+  var fullName = '$firstName $lastName';
+
+  print(fullName);
+
+  var text = 'this is \'dart\' \$cool';
+  print(text);
+
+  var name1 = 'Taufan' 'Prasetyo' 'Basri';
+  print(name1);
+
+  var longString = '''
+    string ini sangat panjang
+    sehingga sulit dibuat dalam
+    satu bari kode program
+  ''';
+  print(longString);
+}

c8f50cd32284c370d6de56570bf81bac3b020c6f
Taufan Prasetyo <taufanremote@gmail.com>
2024-08-21 18:49:33 +0700

    Function short expression

--- bin/function.dart
+++ bin/function.dart
@@ -5,7 +5,9 @@ void main() {
   sayHello2(firstName: 'Taufan');
   sayHello2(lastName: 'Karlina', firstName: 'Evita');
 
-  print(sum([1,2,3,4,5]));
+  print(sum([1, 2, 3, 4, 5]));
+
+  print(sum2(10, 10));
 }
 
 void sayHello(String firstName, [String? lastName = '']) {
@@ -16,12 +18,14 @@ void sayHello2({required String firstName, String lastName = ''}) {
   print('Hello $firstName $lastName');
 }
 
-int sum(List<int> numbers){
+int sum(List<int> numbers) {
   var total = 0;
 
-  for(var value in numbers){
+  for (var value in numbers) {
     total += value;
   }
 
   return total;
 }
+
+int sum2(int first, int second) => first + second;

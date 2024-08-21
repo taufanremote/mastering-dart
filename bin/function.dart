176ff5ed67e3933802a5ed0d8ec28b9ecdf62ef6
Taufan Prasetyo <taufanremote@gmail.com>
2024-08-21 18:41:00 +0700

    Function return value

--- bin/function.dart
+++ bin/function.dart
@@ -4,6 +4,8 @@ void main() {
 
   sayHello2(firstName: 'Taufan');
   sayHello2(lastName: 'Karlina', firstName: 'Evita');
+
+  print(sum([1,2,3,4,5]));
 }
 
 void sayHello(String firstName, [String? lastName = '']) {
@@ -12,4 +14,14 @@ void sayHello(String firstName, [String? lastName = '']) {
 
 void sayHello2({required String firstName, String lastName = ''}) {
   print('Hello $firstName $lastName');
+}
+
+int sum(List<int> numbers){
+  var total = 0;
+
+  for(var value in numbers){
+    total += value;
+  }
+
+  return total;
 }
